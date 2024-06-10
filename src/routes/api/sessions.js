e70725ed500fd75fe5eff3b682edfb70fb194ed9
Roberto Méndez Strobl <rmendezstrobl@gmail.com>
2024-06-09 20:21:49 -0400

    implementación HASH

--- src/routes/api/sessions.js
+++ src/routes/api/sessions.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import User from '../../models/user.model.js';
+import { createHash, isValidPassword } from '../../../utils.js';
 
-const router = Router();
+const router = Router(); 
 
 router.post('/register', async (req, res) => {
     const { first_name, last_name, email, age, password } = req.body;
     try {
-        const newUser = new User({ first_name, last_name, email, age, password });
+        const newUser = new User({ first_name, last_name, email, age, password:createHash(password) });
         await newUser.save();
         res.redirect('/login');
     } catch (err) {
@@ -16,11 +17,14 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
+    if(!email || !password) return res.status(400).send({status:"error", error:"Datos incompletos"})
     console.log(email, password)
     try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }, {email:1, first_name:1, last_name:1, password:1, age:1});
         console.log(user)
         if (!user) return res.status(404).send('Usuario no encontrado');
+        if(!isValidPassword(user, password)) return res.status(403).send({status: "error", error: "Password incorrecto"})
+            delete user.password
         req.session.user = {
             id: user._id,
             first_name: user.first_name,
--- utils.js
+++ utils.js
@@ -1,5 +1,10 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import bcrypt from 'bcryptjs'
+
+export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password)
 
 // __filename and __dirname aren't available by default in ES modules, so we need to create them
 const __filename = fileURLToPath(import.meta.url);
