d5e6bfa780b33040649eadfa9b103a1c7ff394e3
Roberto Méndez Strobl <rmendezstrobl@gmail.com>
2024-06-24 21:19:36 -0400

    test

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -4,7 +4,7 @@ import GitHubStrategy from 'passport-github2';
 import usersModel from '../models/user.model.js'
 import { createHash, isValidPassword } from "../utils.js";
 import dotenv from 'dotenv';
-// Cargar variables de entorno
+// Cargar variables de entorno test
 dotenv.config();
 const userService = usersModel;
 const LocalStrategy = local.Strategy
